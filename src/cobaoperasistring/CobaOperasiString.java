b4ba1adfa99ba93c917c1285d61b030f66be1dac
WINDOWS 10 <WINDOWS 10@DESKTOP-NE2FS30>
2018-08-13 18:58:34 +0700

    Initial Commit

---
+++ src/cobaoperasistring/CobaOperasiString.java
@@ -0,0 +1,8 @@
+package cobaoperasistring;
+public class CobaOperasiString {
+    public static void main(String[] args) {
+        String identitas = "Taufiqy Firdaus Jatu/ XRPL2/ 36";
+        System.out.println("identitas : "+ identitas);
+    }
+    
+}
