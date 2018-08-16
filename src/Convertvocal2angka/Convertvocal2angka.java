4749e81d7c4459e0e56408c43385bcaecc9c4341
WINDOWS 10 <WINDOWS 10@DESKTOP-NE2FS30>
2018-08-16 20:08:12 +0700

    Tambah Tampil Hasil

--- src/Convertvocal2angka/Convertvocal2angka.java
+++ src/Convertvocal2angka/Convertvocal2angka.java
@@ -32,4 +32,18 @@ public class Convertvocal2angka {
         for(int i=0;i<arrKal.length;i++)
             System.out.println(arrKal[i]+" => " + arrCon[i]);
     }
+    private static void tampilHasil(String convert)
+    {
+        System.out.println("Kalimat Alay Angka : " + convert);
+    }
+    public static void main(String[] args) {
+        
+        String identitas = "Taufiqy Firdaus Jatu / XRPL2 / 36";
+        
+        tampilJudul(identitas);   
+        String kalimat = tampilInput();
+        String convert = vocal2Angka(kalimat);
+        tampilPerKata(kalimat, convert);
+        tampilHasil(convert);
+}
 }
