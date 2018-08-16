9fa900dc4b2b4a5e71f988a267c8e8b9b9e03713
WINDOWS 10 <WINDOWS 10@DESKTOP-NE2FS30>
2018-08-16 19:35:43 +0700

    Tambah Tampil Hasil

--- src/convertvocal2angka/ConvertVocal2angka.java
+++ src/convertvocal2angka/ConvertVocal2angka.java
@@ -32,4 +32,8 @@ public class ConvertVocal2angka {
         for(int i=0;i<arrKal.length;i++)
             System.out.println(arrKal[i]+" => " + arrCon[i]);
     }
+    private static void tampilHasil(String convert)
+    {
+        System.out.println("Kalimat Alay Angka : " + convert);
+    }
 }
