9c37b60262867187ad6632f9ffd158efd5364960
WINDOWS 10 <WINDOWS 10@DESKTOP-NE2FS30>
2018-08-16 20:02:58 +0700

    Tambah Input

--- src/Convertvocal2angka/Convertvocal2angka.java
+++ src/Convertvocal2angka/Convertvocal2angka.java
@@ -6,4 +6,13 @@ public class Convertvocal2angka {
         
         System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");
     }
+    public static String tampilInput() {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Masukkan Kalimat: ");
+        String kalimat = scanner.nextLine();
+        System.out.println("Kalimat Asli : " + kalimat);
+        
+        return kalimat;
+    }
 }
